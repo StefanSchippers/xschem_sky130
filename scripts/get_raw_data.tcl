ea875f72d9290ba34443ee8f440ad904882ead18
stefan schippers <stefan.schippers@gmail.com>
2024-04-19 23:30:25 +0200

    added comments on scripts/get_raw_data.tcl

--- scripts/get_raw_data.tcl
+++ scripts/get_raw_data.tcl
@@ -1,3 +1,13 @@
+# get a selected set of variables from a simulation raw file and save into a csv file
+#    rawile: full path of the simulation raw file to load
+#    type: simulation type (tran, op, dc, ac, noise, ...)
+#    vars: a list of variables to extract like: {time vcc clk enab out}
+#    dset: simulation run (in case of multiple runs) to load. Dataset numbers start from 0
+#    a value of -1 or no value at all will load all dataset one after the other.
+#    sep: separator for the csv file
+# example:
+#    get_raw_data $netlist_dir/solar_panel.raw tran {time led panel i(vled)}
+
 proc get_raw_data {rawfile type vars {dset -1} {sep ,}} {
   set res {}
   xschem raw read $rawfile $type
